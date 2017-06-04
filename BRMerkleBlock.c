0fd58e4c8e74a5b6a404569c9f8a23ce0a6bbb88
hugohn <hugh.hn@gmail.com>
2017-06-04 14:47:42 -0700

    correct max target to 0x1e0fffff

--- BRMerkleBlock.c
+++ BRMerkleBlock.c
@@ -31,7 +31,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define MAX_PROOF_OF_WORK 0x1e0ffff0    // highest value for difficulty target (higher values are less difficult)
+#define MAX_PROOF_OF_WORK 0x1e0fffff    // highest value for difficulty target (higher values are less difficult)
 #define TARGET_TIMESPAN   302400        // = 3.5*24*60*60; the targeted timespan between difficulty target adjustments
 
 inline static int _ceil_log2(int x)
