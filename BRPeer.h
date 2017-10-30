4440871f3f13a3579455d73cdc0db28ab3ff1bb1
Loshan T <losh11@users.noreply.github.com>
2017-10-30 05:31:05 +0000

    Bump BR_VERSION

--- BRPeer.h
+++ BRPeer.h
@@ -62,7 +62,7 @@ extern "C" {
 #define SERVICES_NODE_BLOOM   0x04 // BIP111: https://github.com/bitcoin/bips/blob/master/bip-0111.mediawiki
 #define SERVICES_NODE_BCASH   0x20 // https://github.com/Bitcoin-UAHF/spec/blob/master/uahf-technical-spec.md
 
-#define BR_VERSION "1.0.0"
+#define BR_VERSION "2.0.0"
 #define USER_AGENT "/loaf:" BR_VERSION "/"
 
 // explanation of message types at: https://en.bitcoin.it/wiki/Protocol_specification
