5b0877b2d73a6973ee4940600691d1f6c8ced8ed
Neil Vallon <neilvallon@gmail.com>
2018-03-10 00:42:57 -0600

    core: fix useragent and checkpoints for the last time

--- Modules/breadwallet-core/BRPeer.h
+++ Modules/breadwallet-core/BRPeer.h
@@ -62,8 +62,8 @@ extern "C" {
 #define SERVICES_NODE_BLOOM   0x04 // BIP111: https://github.com/bitcoin/bips/blob/master/bip-0111.mediawiki
 #define SERVICES_NODE_BCASH   0x20 // https://github.com/Bitcoin-UAHF/spec/blob/master/uahf-technical-spec.md
 
-#define BR_VERSION "1.77.6"
-#define USER_AGENT "/Mhoy Money:" BR_VERSION ""
+#define BR_VERSION "2.0.4"
+#define USER_AGENT "/FAKit:" BR_VERSION ""
 
 // explanation of message types at: https://en.bitcoin.it/wiki/Protocol_specification
 #define MSG_VERSION     "version"
